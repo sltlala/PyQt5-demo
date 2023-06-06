4ed1d9b9cc80fa302974af59c6f82d031e22b62f
sltlala <sltlala@163.com>
2023-06-06 10:53:16 +0800

    更新

--- ui/main2.py
+++ ui/main2.py
@@ -5,12 +5,12 @@
 
 from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
-import Qt_ui.FristMain
+import Qt_ui.Home
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)  # 创建应用程序对象
     MainWindow = QMainWindow()  # 创建主窗口
-    ui = Qt_ui.FristMain.Ui_MainWindow()
+    ui = Qt_ui.Home.Ui_MainWindow()
     ui.setupUi(MainWindow)
     MainWindow.show()  # 显示主窗口
     sys.exit(app.exec_())  # 在主线程中退出
